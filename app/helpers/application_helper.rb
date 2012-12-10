5d022ae56024bf59bb29ad3df89634bd7d08abda
carlos silva <cefs1984@gmail.com>
2012-12-10 01:09:10 -0300

    finalized chapter 7.9

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+	def error_tag(model, attribute)
+		if model.errors.has_key? attribute
+			content_tag :div,  model.errors[attribute].first, :class => 'error_message'
+		end
+	end
+
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,9 @@
 class Room < ActiveRecord::Base
   attr_accessible :description, :location, :title
 
+  validates_presence_of :title, :location
+  validates_length_of :description, :minimum => 30, :allow_blank => false
+
 	def complete_name
 		"#{title} #{location}"
 	end
