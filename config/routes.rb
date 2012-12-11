e3080500602e3e07a790da12a00850eeb9112bc8
carlos silva <cefs1984@gmail.com>
2012-12-11 15:22:20 -0300

    finalized chapter 8.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Colchonet::Application.routes.draw do
-  resources :rooms
-  resources :users
 
-  root :to =>"home#index"
+	LOCALES = /en|pt\-BR/
+
+	scope "(:locale)", :locale => LOCALES do
+	  resources :rooms
+	  resources :users
+
+	  resource :user_confirmation, :only => [:show]
+ 	end
+
+ 	match '/:locale' => 'home#index', :locale => LOCALES
+
+ 	root :to =>"home#index"
+
 end
