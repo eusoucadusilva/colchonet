e187268fa2bc027e994f2c0330b71fb380198db7
CARLOS SILVA <cefs1984@gmail.com>
2012-12-12 02:05:13 -0300

    finalized sub chapter 10.3

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,10 +1,16 @@
 class UserSessionsController < ApplicationController
 
 	def new
-			@session = UserSession.new
+			@session = UserSession.new(session)
 	end
 
 	def create
+		@session = UserSession.new(session,params[:user_session])
+		if  @session.authenticate
+			redirect_to root_path, :notice => t('flash.notice.signed_in') 
+		else
+			render :new
+		end
 	end
 
 	def destroy
--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -8,6 +8,29 @@ class UserSession
 
 	attr_accessor :email, :password
 
+
+	def initialize(session, attributes ={})
+		@session = session
+		@email   = attributes[:email]
+		@password= attributes[:password]
+	end
+
+	def store(user)
+		@session[:user_id] = user.id
+	end
+
+	def authenticate
+		user = User.authenticate(@email,@password)
+
+		if user.present?
+			store(user)
+		else
+			errors.add(:base, :invalid_login)
+			false
+		end
+	end
+
+
 	validates_presence_of :email, :password
 
 	def persisted?
