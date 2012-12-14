e51cdc727a43671d388134f881ea8afea8a25729
CARLOS SILVA <cefs1984@gmail.com>
2012-12-14 00:46:01 -0300

    finalized sub chapter 11.6

--- app/models/room.rb
+++ app/models/room.rb
@@ -6,7 +6,7 @@ class Room < ActiveRecord::Base
 	validates_presence_of :title, :location
 	validates_length_of :description, :minimum => 30, :allow_blank => false
 
-	scope :romm_recent, order('create_at desc')
+	scope :most_recent, order('created_at desc')
 
 	def complete_name
 		"#{title} #{location}"
