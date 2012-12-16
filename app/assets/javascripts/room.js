d539a54a6c9b2e5f2ac15e9344e87ddf802ddaf2
CARLOS SILVA <cefs1984@gmail.com>
2012-12-16 20:26:39 -0300

    finalized sub chapter 12.5

---
+++ app/assets/javascripts/room.js
@@ -0,0 +1,24 @@
+$(function(){
+	var $review = $('.review');
+
+	$review.bind('ajax:beforeSend',function(){
+		$(this).find('input').attr('disabled',true);
+	});
+
+	$review.bind('ajax:error',function(){
+		replaceButton(this, 'icon-remove','#B94A48');
+	});
+
+	$review.bind('ajax:success', function(){
+		replaceButton(this, 'icon-ok', '#468847');
+	})
+
+	function replaceButton(container, icon_class, color){
+		$(container).find('input:submit').
+		replaceWith(
+					$('<i/>')
+					).
+			addClass(icon_class).
+			css('color', color);
+	};
+});
